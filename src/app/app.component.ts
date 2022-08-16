eaa1cc87060f2021cceb8c00ddc4a7fb3168d46e
karolkolodziej <karolkolodziej01@gmail.com>
2022-08-16 16:16:45 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+// @ts-ignore
+import * as Highcharts from 'highcharts'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'hclocal';
+  ngOnInit(){
+    Highcharts.chart({
+      chart:{
+        renderTo:'container'
+      },
+      series: [{
+        type:'line',
+        data: [2, 5, 1, 6, 7, 8, 5]
+      }],
+    });
+  }
 }
